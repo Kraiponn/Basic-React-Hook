9a3daccfbdb8b0d2b8dbb3814893fc8363e232ef
kraiponn <kraipon.na10@gmail.com>
2019-09-24 00:28:11 +0700

    Project v1

---
+++ src/hooks-store/store.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export const useStore = () => {
+    const setState = useState(globalState)[1];
+    
+    const dispatch = (actionIdentifier, payload) => {
+        const newState = action[actionIdentifier](globalState, payload);
+        globalState = {...globalState, ...newState};
+
+        for(let listener of listeners){
+            listener(globalState);
+        }
+    }
+
+    useEffect(() => {
+        listeners.push(setState);
+        return () => {
+            listeners = listeners.filter(li => li !== setState);
+        };
+    }, [setState]);
+
+    return [globalState, dispatch];
+};
+
+export const initStore = (useActions, initialState) => {
+    if(initialState){
+        globalState = {...globalState, ...initialState};
+    }
+
+    actions = {...actions, ...useActions};
+};
